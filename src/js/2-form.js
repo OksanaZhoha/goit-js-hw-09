852b733c8f3f4bb75b31ba13553704816222a661
OksanaZhoha <zhoga.oksana@gmail.com>
2024-02-11 16:33:25 +0200

    Create 2-form.js

---
+++ src/js/2-form.js
@@ -0,0 +1,37 @@
+const localStorageKey = 'feedback-form-state';
+
+const formEl = document.querySelector('.feedback-form');
+const emailInput = formEl.elements.email;
+const messageTextarea = formEl.elements.message;
+
+formEl.addEventListener('input', onInputValue);
+
+function onInputValue(evt) {
+  evt.preventDefault();
+  const userinfo = {
+    email: emailInput.value.trim(),
+    message: messageTextarea.value.trim(),
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(userinfo));
+}
+
+const rowData = localStorage.getItem(localStorageKey);
+if (rowData !== null) {
+  const data = JSON.parse(rowData);
+  emailInput.value = data.email;
+  messageTextarea.value = data.message;
+}
+
+formEl.addEventListener('submit', onClickBtn);
+
+function onClickBtn(evt) {
+  evt.preventDefault();
+  const email = emailInput.value.trim();
+  const message = messageTextarea.value.trim();
+  if (email === '' || message === '') {
+    return;
+  }
+  console.log(JSON.parse(localStorage.getItem(localStorageKey)));
+  localStorage.removeItem(localStorageKey);
+  formEl.reset();
+}
